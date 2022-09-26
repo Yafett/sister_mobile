4c038bd422d6279702ff0dc4cca07977d06cef46
Yafett <exdeath350@gmail.com>
2022-09-26 16:00:16 +0700

    membuat-register-2

--- lib/pages/register-page.dart
+++ lib/pages/register-page.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:sister_mobile/shared/theme.dart';
+import 'package:sister_mobile/widget/no_scroll_waves.dart';
 
 class RegisterPage extends StatelessWidget {
   const RegisterPage({Key? key}) : super(key: key);
@@ -9,11 +10,13 @@ class RegisterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var unitList = ['', ''];
+    var genderList = ['Boy', 'Girl', 'Other'];
     var unitval;
-    return _buildRegisterPage(context, unitList, unitval);
+    return _buildRegisterPage(context, unitList, genderList, unitval);
   }
 
-  Widget _buildRegisterPage(context, List<String> unitList, unitval) {
+  Widget _buildRegisterPage(
+      context, List<String> unitList, genderList, unitval) {
     return Scaffold(
         backgroundColor: const Color(0xffE8E8E8),
         appBar: AppBar(
@@ -38,12 +41,16 @@ class RegisterPage extends StatelessWidget {
                 ],
               ),
             )),
-        body: SingleChildScrollView(
-            child: Column(
-          children: [
-            _buildUnitInformation(context, unitList, unitval),
-          ],
-        )));
+        body: ScrollConfiguration(
+          behavior: NoScrollWaves(),
+          child: SingleChildScrollView(
+              child: Column(
+            children: [
+              _buildUnitInformation(context, unitList, unitval),
+              _buildStudentInformation(context, genderList, unitval),
+            ],
+          )),
+        ));
   }
 
   Widget _buildUnitInformation(context, List<String> unitList, unitval) {
@@ -167,6 +174,255 @@ class RegisterPage extends StatelessWidget {
                 hintStyle: fGreyTextStyle,
               ),
             ),
+          ],
+        ));
+  }
+
+  Widget _buildStudentInformation(context, List<String> genderList, unitval) {
+    return Container(
+        padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 15),
+        width: MediaQuery.of(context).size.width,
+        margin: const EdgeInsets.only(top: 30),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: const Color(0XFFF8F9FA),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Unit Information',
+              style: fBlackTextStyle.copyWith(
+                fontSize: 20,
+              ),
+            ),
+            const Divider(thickness: 1),
+            const SizedBox(height: 15),
+
+            // ! Fullname Field
+            Text('Fullname', style: fTextColorStyle),
+            const SizedBox(height: 5),
+            TextFormField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(color: Color(0XFFE8E8E8)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(color: Color(0XFFE8E8E8)),
+                ),
+                hintText: 'e.x John Doe',
+                hintStyle: fGreyTextStyle,
+              ),
+            ),
+            const SizedBox(height: 15),
+
+            // ! Nickname Field
+            Text('Nickname', style: fTextColorStyle),
+            const SizedBox(height: 5),
+            TextFormField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(color: Color(0XFFE8E8E8)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(color: Color(0XFFE8E8E8)),
+                ),
+                hintText: 'e.x John',
+                hintStyle: fGreyTextStyle,
+              ),
+            ),
+            const SizedBox(height: 15),
+
+            // ! Email Field
+            Text('Email', style: fTextColorStyle),
+            const SizedBox(height: 5),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(color: Color(0XFFE8E8E8)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(color: Color(0XFFE8E8E8)),
+                ),
+                hintText: 'e.x john@example.com',
+                hintStyle: fGreyTextStyle,
+              ),
+            ),
+            const SizedBox(height: 15),
+
+            // ! Whatsapp Field
+            Text('Whatsapp Number', style: fTextColorStyle),
+            const SizedBox(height: 5),
+            TextFormField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(color: Color(0XFFE8E8E8)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(color: Color(0XFFE8E8E8)),
+                ),
+                hintText: 'e.x 0821xxxxx',
+                hintStyle: fGreyTextStyle,
+              ),
+            ),
+            const SizedBox(height: 15),
+
+            // ! Gender Field
+            Text('Gender', style: fTextColorStyle),
+            Container(
+              margin: const EdgeInsets.only(top: 5),
+              padding: const EdgeInsets.symmetric(vertical: 3, horizontal: 10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: const Color(0XFFE8E8E8)),
+                borderRadius: const BorderRadius.all(Radius.circular(8)),
+              ),
+              width: MediaQuery.of(context).size.width,
+              child: DropdownButton(
+                underline: const SizedBox(),
+                isExpanded: true,
+                hint: Text('e.x boy', style: fGreyTextStyle),
+                items: genderList.map((item) {
+                  return DropdownMenuItem(
+                    value: item.toString(),
+                    child: Text(item.toString()),
+                  );
+                }).toList(),
+                onChanged: (newVal) {},
+                value: unitval,
+              ),
+            ),
+            const SizedBox(height: 15),
+
+            // ! Birth Date Field
+            Text('Birth Date', style: fTextColorStyle),
+            TextFormField(
+              readOnly: true,
+              decoration: InputDecoration(
+                suffixIcon: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.date_range,
+                      color: Color(0xff616161),
+                    )),
+                filled: true,
+                fillColor: Colors.white,
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(color: Color(0XFFE8E8E8)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(color: Color(0XFFE8E8E8)),
+                ),
+                hintText: 'e.x dd:mm:yyyy',
+                hintStyle: fGreyTextStyle,
+              ),
+            ),
+            const SizedBox(height: 15),
+
+            // ! Birth Place Field
+            Text('Birth Place', style: fTextColorStyle),
+            const SizedBox(height: 5),
+            TextFormField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(color: Color(0XFFE8E8E8)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(color: Color(0XFFE8E8E8)),
+                ),
+                hintText: 'e.x place',
+                hintStyle: fGreyTextStyle,
+              ),
+            ),
+            const SizedBox(height: 15),
+
+            // ! Address Field
+            Text('Address', style: fTextColorStyle),
+            const SizedBox(height: 5),
+            TextFormField(
+              keyboardType: TextInputType.streetAddress,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(color: Color(0XFFE8E8E8)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(color: Color(0XFFE8E8E8)),
+                ),
+                hintText: 'e.x jl.xxxxx',
+                hintStyle: fGreyTextStyle,
+              ),
+            ),
+            const SizedBox(height: 15),
+
+            // ! City Field
+            Text('City', style: fTextColorStyle),
+            const SizedBox(height: 5),
+            TextFormField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(color: Color(0XFFE8E8E8)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(color: Color(0XFFE8E8E8)),
+                ),
+                hintText: 'e.x city',
+                hintStyle: fGreyTextStyle,
+              ),
+            ),
+            const SizedBox(height: 15),
+
+            // ! Religion Field
+            Text('Religion', style: fTextColorStyle),
+            const SizedBox(height: 5),
+            TextFormField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(color: Color(0XFFE8E8E8)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(color: Color(0XFFE8E8E8)),
+                ),
+                hintText: 'e.x religion',
+                hintStyle: fGreyTextStyle,
+              ),
+            ),
+            const SizedBox(height: 15),
           ],
         ));
   }
